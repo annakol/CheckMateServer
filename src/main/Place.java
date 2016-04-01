dcd05b80d8965e7f9a010c3c8098555e38c64466
Anna <ankol24@gmail.com>
2016-04-01 23:19:20 +0300

    Clean code and change servlet so it returns the location result

--- src/main/Place.java
+++
@@ -1,8 +0,0 @@
-package main;
-
-public class Place {
-	
-	public String name;
-	public String reference;
-
-}
---
+++ src/module/Place.java
@@ -0,0 +1,12 @@
+package module;
+
+import java.util.List;
+
+public class Place {
+	
+	public String id;
+	public String name;
+	public List<String> type; 
+	public Location location;
+
+}
